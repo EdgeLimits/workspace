5838be701cb58735be21d246c774b3786bf05f30
EdgeLimits <edgars.kru@gmail.com>
2023-12-11 09:05:12 +0200

    chore(nvim): nvim-treesitter

--- config/nvim/README.md
+++ config/nvim/README.md
@@ -7,3 +7,13 @@
 ## Tutorials:
 
 https://www.youtube.com/watch?v=X6AR2RMB5tE
+
+https://www.youtube.com/watch?v=6mxWayq-s9I
+https://www.youtube.com/watch?v=zHTeCSVAFNY
+
+Python
+https://www.youtube.com/watch?v=4BnVeOUeZxc
+
+Repo to inspire from:
+https://github.com/josean-dev/dev-environment-files/tree/main/.config/nvim/lua/josean/plugins
+https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
---
+++ config/nvim/lua/edgelimits/plugins/nvim-treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    treesitter.setup({
+      ensure_installed = {"lua", "javascript", "typescript", "python"},
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
