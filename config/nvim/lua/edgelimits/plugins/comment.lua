d370bc96f8211b3572c6bc0f89f231afdf98a8f5
Edge <edgars.kru@gmail.com>
2023-12-14 12:12:54 +0100

    feature(nvim): add lualine

--- config/nvim/lua/edgelimits/plugins/comment.lua
+++ config/nvim/lua/edgelimits/plugins/comment.lua
@@ -3,3 +3,13 @@ return{
   event = {  "BufReadPre", "BufNewFile" },
   config = true -- runs require("Comment").setup()
 }
+
+-- NORMAL mode
+--[[
+`gcc` - Toggles the current line using linewise comment
+`gbc` - Toggles the current line using blockwise comment
+`[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+`[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+`gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+`gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment ]]
+
---
+++ config/nvim/lua/edgelimits/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = "dracula",
+      component_separators = "|",
+      seciont_separatos = "",
+    }
+  }
+}
