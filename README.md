f825874c639a20b0f7e023aecc08ca10ef2e6e18
Edge <edgars.kru@gmail.com>
2023-12-21 18:31:01 +0200

    readme

--- README.md
+++ README.md
@@ -59,3 +59,10 @@ Split panes vertically: <leader> -
 Split panes horizontally: <leader> |
 
 Reload tmux config: <leader> r
+```
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
+defaults write com.vscodium ApplePressAndHoldEnabled -bool false                      # For VS Codium
+defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
+defaults delete -g ApplePressAndHoldEnabled
+```
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -28,14 +28,15 @@ return{
   branch = "0.1.x",
   dependencies = {
     'nvim-lua/plenary.nvim',
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
+    -- "nvim-treesitter/nvim-treesitter",
+    -- "nvim-tree/nvim-web-devicons",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "neovim/nvim-lspconfig",
+    -- "neovim/nvim-lspconfig",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
     telescope.setup({
       defaults = {
@@ -52,9 +53,10 @@ return{
     })
 
     telescope.load_extension("fzf")
-
+    
+    vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files in cwd" })
     -- set keymaps 
-    vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    -- vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
