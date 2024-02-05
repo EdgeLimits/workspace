92f05433ac0da96a81ac46315f50b46700252c42
Edge <edgars.kru@gmail.com>
2024-02-05 10:15:53 +0200

    better telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 -- brew install ripgrep
+-- Large JSON file solution: https://github.com/nvim-telescope/telescope.nvim/issues/269
 -- 
 return{
   'nvim-telescope/telescope.nvim',
@@ -8,16 +9,37 @@ return{
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    -- "neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
     local builtin = require("telescope.builtin")
 
+    local previewers = require('telescope.previewers')
+
+    local _bad = { '.*%.json', '.*%.lua' } -- Put all filetypes that slow you down in this array
+    local bad_files = function(filepath)
+      for _, v in ipairs(_bad) do
+        if filepath:match(v) then
+          return false
+        end
+      end
+
+      return true
+    end
+
+    local new_maker = function(filepath, bufnr, opts)
+      opts = opts or {}
+      if opts.use_ft_detect == nil then opts.use_ft_detect = true end
+      opts.use_ft_detect = opts.use_ft_detect == false and false or bad_files(filepath)
+      previewers.buffer_previewer_maker(filepath, bufnr, opts)
+    end
+
     telescope.setup({
       defaults = {
         file_ignore_patterns = {},
+        buffer_previewer_maker = new_maker,
         path_display = { "truncate" },
         mappings = {
           i = {
