5ad52168f495cab433b6ab76b00fc239e8b7f3d9
EdgeLimits <edgars.kru@gmail.com>
2024-02-08 20:19:16 +0200

    harppon

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -11,8 +11,8 @@ return {
       }
     })
 
-    vim.keymap.set("n", "<leader>A", function() harpoon:list():append() end, { desc = "Harpoon add file" })
-    vim.keymap.set("n", "<leader>a", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toogle quick menu" })
+    vim.keymap.set("n", "<leader>H", function() harpoon:list():append() end, { desc = "Harpoon add file" })
+    vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toogle quick menu" })
     vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon to file 1" })
     vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon to file 2" })
     vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon to file 3" })
