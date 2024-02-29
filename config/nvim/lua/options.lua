08ac190595e05cde7f9ae45301156cca2f3cf832
Edge <edgars.kru@gmail.com>
2024-02-29 07:06:00 +0200

    vimrc

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,6 +1,39 @@
 local opt = vim.opt
 
-opt.clipboard = "unnamedplus" -- Sync the system clipboard with Neovim
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.opt.clipboard = 'unnamedplus'
+--
+-- -- Enable break indent
+-- vim.opt.breakindent = true
+--
+-- -- Save undo history
+-- vim.opt.undofile = true
+--
+-- -- Case-insensitive searching UNLESS \C or capital in search
+-- vim.opt.ignorecase = true
+-- vim.opt.smartcase = true
+--
+-- -- Keep signcolumn on by default
+-- vim.opt.signcolumn = 'yes'
+--
+-- -- Decrease update time
+-- vim.opt.updatetime = 250
+-- vim.opt.timeoutlen = 300
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+--
+-- -- Preview substitutions live, as you type!
+-- vim.opt.inccommand = 'split'
+--
+-- -- Show which line your cursor is on
+-- vim.opt.cursorline = true
+--
+-- -- Minimal number of screen lines to keep above and below the cursor.
+-- vim.opt.scrolloff = 10
+
+
 
 -- opt.hlsearch = false -- Set highlight on search
 -- opt.laststatus = 3 -- Enable the global status line
--- config/nvim/vimrc.vim
+++ config/nvim/vimrc.vim
@@ -18,9 +18,11 @@ set relativenumber
 
 " Automatically indent on newline
 set autoindent
+set breakindent
 
 " Ignore case when searching
 set ignorecase
+set smartcase
 
 " Always show status line
 set laststatus=2
@@ -28,7 +30,22 @@ set laststatus=2
 " Enable mouse mode
 set mouse=a
 
+set undofile
+
+set ignorecase
+set smartcase
+
+set signcolumn
+
+set updatetime=250
+set timeoutlen=300
+
+set splitright
+set splitbelow
+
+set list
+set listchars=tab:»\ ,extends:›,precedes:‹,nbsp:·,trail:·
 "" Better movement through wrapped lines
-noremap j gj
-noremap k gk
+" noremap j gj
+" noremap k gk
 
