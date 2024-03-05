5373114dd0a165f6f80bd6eac49e4f35bafa06aa
Edge <edgars.kru@gmail.com>
2024-03-05 10:51:04 +0200

    list chars

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -49,3 +49,18 @@ end
 vim.o.termguicolors = true
 vim.cmd.colorscheme "catppuccin-macchiato"
 
+-- Set listchars
+vim.o.listchars = 'nbsp:␣,eol:↲,tab:»\\ ,extends:›,precedes:‹,trail:•'
+
+-- Set showbreak
+vim.o.showbreak = '↳ '
+
+-- Disable number column in visual mode
+vim.api.nvim_exec([[
+  augroup my_visuallistchars
+    autocmd!
+    autocmd CursorMoved * if mode() =~# "[vV\<C-v>]" | set list | else | set nolist | endif
+  augroup END
+]], false)
+
+
