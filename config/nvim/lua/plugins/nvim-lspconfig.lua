32899bd79d6c2dcda6e1d7b31436fdfa1f2b3bdf
EdgeLimits <edgars.kru@gmail.com>
2024-03-08 09:38:15 +0200

    lsp diagnostic icons

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -78,6 +78,12 @@ return { -- LSP Configuration & Plugins
             callback = vim.lsp.buf.clear_references,
           })
         end
+
+        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        for type, icon in pairs(signs) do
+          local hl = "DiagnosticSign" .. type
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
       end,
     })
 
