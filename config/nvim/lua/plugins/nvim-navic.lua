6c40918735051b5d5315d7ce0f54ad6d183a51d6
Edge <edgars.kru@gmail.com>
2024-03-15 16:30:19 +0200

    trying to add navic

---
+++ config/nvim/lua/plugins/nvim-navic.lua
@@ -0,0 +1,17 @@
+return {
+  "SmiteshP/nvim-navic",
+  module = "nvim-navic",
+  requires = { "neovim/nvim-lspconfig", module = "lspconfig" },
+  config = function()
+
+    local navic = require("nvim-navic")
+    navic.setup({})
+
+    require("lspconfig").clangd.setup {
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+}
+
+  end,
+}
