0ad777d1e28c94847c24d5b6796f971e56abf00a
Edge <edgars.kru@gmail.com>
2024-04-04 17:12:46 +0300

    history

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,3 +1,18 @@
+-- Set listchars
+vim.o.listchars = 'nbsp:␣,eol:↲,tab:»\\ ,extends:›,precedes:‹,trail:•'
+
+-- Set showbreak
+vim.o.showbreak = '↳ '
+
+-- Disable number column in visual mode
+vim.api.nvim_exec([[
+  augroup my_visuallistchars
+    autocmd!
+    autocmd CursorMoved * if mode() =~# "[vV\<C-v>]" | set list | else | set nolist | endif
+  augroup END
+]], false)
+
+
 -- Native (Vim / Nvim / Netrw) keymaps
 
 -- Keymaps for better default experience
