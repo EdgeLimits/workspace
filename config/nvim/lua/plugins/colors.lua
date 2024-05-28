eaef624f9c80ea4b4729c15f54b0088447d00fe8
Edge <edgars.kru@gmail.com>
2024-05-28 09:27:29 +0300

    color improvements

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,15 +1,108 @@
 -- https://github.com/catppuccin/nvim
+-- inpsiration: https://github.com/augustocdias/dotfiles/blob/main/.config/nvim/lua/setup/catppuccin.lua
 return {
   "catppuccin/nvim", name = "catppuccin", priority = 1000,
   config = function ()
     local catppuccin = require("catppuccin");
+    local colors = require("catppuccin.palettes").get_palette()
+    local ucolors = require('catppuccin.utils.colors')
+    local lualine_bg = colors.mantle
+    local telescope_prompt = ucolors.darken(colors.crust, 0.95, '#000000')
+    local telescope_results = ucolors.darken(colors.mantle, 0.95, '#000000')
+    local telescope_text = colors.text
+    local telescope_prompt_title = colors.sky
+    local telescope_preview_title = colors.teal
     catppuccin.setup({
       integrations = {
+        aerial = true,
+        barbar = true,
         cmp = true,
+        dap = {
+          enabled = true,
+          enable_ui = true,
+        },
+        dashboard = true,
+        fidget = true,
+        flash = true,
+        gitgutter = true,
         gitsigns = true,
-        nvimtree = true,
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = false,
+        },
+        leap = true,
+        lsp_trouble = true,
+        markdown = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { 'italic' },
+            hints = { 'italic' },
+            warnings = { 'italic' },
+            information = { 'italic' },
+          },
+          underlines = {
+            errors = { 'underline' },
+            hints = { 'underline' },
+            warnings = { 'underline' },
+            information = { 'underline' },
+          },
+        },
+        navic = {
+          enabled = true,
+          custom_bg = lualine_bg,
+        },
+        neogit = true,
+        neotest = true,
+        neotree = {
+          enabled = true,
+          show_root = true,
+          transparent_panel = false,
+        },
+        noice = true,
+        notify = true,
+        octo = true,
+        overseer = true,
+        symbols_outline = true,
+        telescope = true,
         treesitter = true,
-      }
+        treesitter_context = true,
+        which_key = true,
+      },
+      highlight_overrides = {
+        all = {
+          -- NoiceCmdlinePopup = { bg = ucolors.lighten(colors.flamingo, 0.1, '#FFFFFF') },
+          -- NoiceMini = { bg = colors.mantle },
+          -- WinBar = { bg = lualine_bg },
+          -- WinBarSigActParm = { fg = colors.blue, bg = lualine_bg },
+          -- WinBarSignature = { fg = colors.flamingo, bg = lualine_bg },
+          -- dims the text so that the hits are more visible
+          -- LeapBackdrop = { fg = colors.flamingo },
+          -- TelescopeBorder = { bg = telescope_results, fg = telescope_results },
+          -- TelescopePromptBorder = { bg = telescope_prompt, fg = telescope_prompt },
+          -- TelescopePromptCounter = { fg = telescope_text },
+          -- TelescopePromptNormal = { fg = telescope_text, bg = telescope_prompt },
+          -- TelescopePromptPrefix = { fg = telescope_prompt_title, bg = telescope_prompt },
+          -- TelescopePromptTitle = { fg = telescope_prompt, bg = telescope_prompt_title },
+          -- TelescopePreviewTitle = { fg = telescope_results, bg = telescope_preview_title },
+          -- TelescopePreviewBorder = {
+          --   bg = ucolors.darken(telescope_results, 0.95, '#000000'),
+          --   fg = ucolors.darken(telescope_results, 0.95, '#000000'),
+          -- },
+          -- TelescopePreviewNormal = {
+          --   bg = ucolors.darken(telescope_results, 0.95, '#000000'),
+          --   fg = telescope_results,
+          -- },
+          -- TelescopeResultsTitle = { fg = telescope_results, bg = telescope_preview_title },
+          -- TelescopeMatching = { fg = telescope_prompt_title },
+          -- TelescopeNormal = { bg = telescope_results },
+          -- TelescopeSelection = { bg = telescope_prompt },
+          -- TelescopeSelectionCaret = { fg = telescope_text },
+          -- TelescopeResultsNormal = { bg = telescope_results },
+          -- TelescopeResultsBorder = { bg = telescope_results, fg = telescope_results },
+          -- NavicIconsFile = { fg = colors.blue, bg = lualine_bg },
+        },
+      },
     })
   end,
 }
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -15,8 +15,7 @@ return{
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-    local builtin = require("telescope.builtin")
-
+    -- local builtin = require("telescope.builtin")
     local previewers = require('telescope.previewers')
 
     local _bad = { '.*%.json', '.*%.lua' } -- Put all filetypes that slow you down in this array
@@ -62,48 +61,17 @@ return{
         grep_string = {
           hidden = true,
         },
+        lsp_references = {
+          theme = "cursor",
+        }
       },
     })
 
 
-    -- local cp = require("catppuccin.palettes").get_palette()
-    -- local TelescopeColor = {
-    --   -- TelescopeBorder = { fg = cp.blue },
-    --   -- TelescopeSelectionCaret = { fg = cp.flamingo },
-    --   -- TelescopeSelection = { fg = cp.text, bg = cp.surface0, bold = true},
-    --   -- TelescopeMatching = { fg = cp.blue },
-    --   -- TelescopePromptPrefix = { bg = cp.crust },
-    --   -- TelescopePromptNormal = { bg = cp.crust},
-    --   -- TelescopeResultsNormal = { bg = cp.mantle},
-    --   -- TelescopePreviewNormal = { bg = cp.crust },
-    --   -- TelescopePromptBorder = { bg = cp.crust, fg = cp.crust },
-    --   -- TelescopeResultsBorder = { bg = cp.mantle, fg = cp.crust },
-    --   -- TelescopePreviewBorder = { bg = cp.crust, fg = cp.crust },
-    --   -- TelescopePromptTitle = { fg = cp.crust },
-    --   -- TelescopeResultsTitle = { fg = cp.text },
-    --   -- TelescopePreviewTitle = { fg = cp.crust },
-    --
-    --   TelescopeMatching = { fg = cp.flamingo },
-    --   TelescopeSelection = { fg = cp.text, bg = cp.surface0, bold = true },
-    --   TelescopePromptPrefix = { bg = cp.surface0 },
-    --   TelescopePromptNormal = { bg = cp.surface0 },
-    --   TelescopeResultsNormal = { bg = cp.mantle },
-    --   TelescopePreviewNormal = { bg = cp.mantle },
-    --   TelescopePromptBorder = { bg = cp.surface0, fg = cp.surface0 },
-    --   TelescopeResultsBorder = { bg = cp.mantle, fg = cp.mantle },
-    --   TelescopePreviewBorder = { bg = cp.mantle, fg = cp.mantle },
-    --   TelescopePromptTitle = { bg = cp.pink, fg = cp.mantle },
-    --   TelescopeResultsTitle = { fg = cp.mantle },
-    --   TelescopePreviewTitle = { bg = cp.green, fg = cp.mantle },
-    -- }
-    --
-    -- for hl, col in pairs(TelescopeColor) do
-    --   vim.api.nvim_set_hl(0, hl, col)
-    -- end
-
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
     -- vim.keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Open recent file" })
     vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string n cwd" })
+    vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
     vim.keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     -- vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "Open a new file" })
     -- vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Open the help tags menu" })
