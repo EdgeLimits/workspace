c08a83f4769cce0e01942938fee3069b29d5b527
EdgeLimits <edgars.kru@gmail.com>
2024-06-13 21:45:17 +0300

    nvim update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- Disable some in-built features which are unnecessary (and probably affects performance?)
