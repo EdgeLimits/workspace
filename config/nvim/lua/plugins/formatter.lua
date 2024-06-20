50b737d20cfeffe52e94953c9049fb2e574e8fdd
Edgars Krumins <ekr@tctadmins-MacBook-Pro.local>
2024-06-20 08:57:49 +0200

    formatting

--- config/nvim/lua/plugins/formatter.lua
+++ config/nvim/lua/plugins/formatter.lua
@@ -1,31 +1,30 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
-        html = { "htmlbeautifier" },
-        python = { "black" },
-        css = { { "prettierd", "prettier" } },
-        scss = { { "prettierd", "prettier" } },
-      },
-    })
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				html = { "htmlbeautifier" },
+				python = { "black" },
+				css = { { "prettierd", "prettier" } },
+				scss = { { "prettierd", "prettier" } },
+			},
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>l", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timout_ms = 500,
-      })
-    end, { desc = "Format file or range (in visual mode)"})
-
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>l", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
