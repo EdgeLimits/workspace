723d32341b52e250bce7286b799bf13def0dcd20
Edgars Krumins <ekr@tctadmins-MacBook-Pro.local>
2024-06-20 08:52:40 +0200

    formatters & linters

---
+++ config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,31 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        html = { "htmlbeautifier" },
+        python = { "black" },
+        css = { { "prettierd", "prettier" } },
+        scss = { { "prettierd", "prettier" } },
+      },
+    })
+
+    vim.keymap.set({ "n", "k" }, "<leader>l", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)"})
+
+  end,
+}
--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -15,15 +15,13 @@ return {
       python = { "pylint" },
     }
 
-
-    local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_autogroup,
-      callback = function ()
-        lint.try_lint()
-      end,
-    })
+    -- local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    --   group = lint_autogroup,
+    --   callback = function ()
+    --     lint.try_lint()
+    --   end,
+    -- })
 
     vim.keymap.set("n", "<leader>ll", function ()
       lint.try_lint()
