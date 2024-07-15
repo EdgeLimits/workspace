e114ef1057e79bfca04fdb56bc69ecafd7db3f1d
Edgars Krumins <ekr@tctadmins-MacBook-Pro.local>
2024-07-15 07:55:41 +0300

    chore: lsp pan for better message display

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -57,6 +57,9 @@ return { -- LSP Configuration & Pluginslsp
         --  For example, in C this would take you to the header
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+        map('<leader>e', vim.diagnostic.open_float, 'Show Lin[e] diagnostic')
+
+
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
