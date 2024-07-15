0ae0d5cab8f2d9900ad458294ab6f2c42bf416b4
Edgars Krumins <ekr@tctadmins-MacBook-Pro.local>
2024-07-15 09:11:25 +0300

    chore: cleanup

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -19,10 +19,6 @@ vim.api.nvim_exec([[
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- window management
-vim.keymap.set("n", "<leader>|", "<cmd>Vex<cr>", { desc = "Split window vertically" }) -- split window vertically
-vim.keymap.set("n", "<leader>-", "<cmd>Hex<cr>", { desc = "Split window horizontally" }) -- split window horizontally
-
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 1 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 1 ? 'gj' : 'j'", { expr = true, silent = true })
