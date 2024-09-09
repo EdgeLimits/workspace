5ca4290f32234955ae3631bd0fafb2dce827184c
EdgeLimits <edgars.kru@gmail.com>
2024-09-09 21:35:39 +0300

    chore: formatter

--- config/nvim/lua/plugins/formatter.lua
+++ config/nvim/lua/plugins/formatter.lua
@@ -1,30 +1,42 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				json = { { "prettierd", "prettier" } },
-				html = { "htmlbeautifier" },
-				python = { "black" },
-				css = { { "prettierd", "prettier" } },
-				scss = { { "prettierd", "prettier" } },
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>lf",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format file or range (in visual mode)",
+		},
+	},
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { { "prettierd", "prettier", stop_after_first = true } },
+			typescript = { { "prettierd", "prettier", stop_after_first = true } },
+			javascriptreact = { { "prettierd", "prettier", stop_after_first = true } },
+			typescriptreact = { { "prettierd", "prettier", stop_after_first = true } },
+			json = { { "prettierd", "prettier", stop_after_first = true } },
+			html = { "htmlbeautifier" },
+			python = { "black" },
+			css = { { "prettierd", "prettier", stop_after_first = true } },
+			scss = { { "prettierd", "prettier", stop_after_first = true } },
+		},
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
+		-- Customize formatters
+		-- formatters = {
+		-- 	shfmt = {
+		-- 		prepend_args = { "-i", "2" },
+		-- 	},
+		-- },
+	},
 }
